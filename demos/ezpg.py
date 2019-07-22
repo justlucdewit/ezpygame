27ba5a241ccb0a6f16b71200cb269204978adbb0
Luc de wit <luc.dewit@hotmail.nl>
2019-07-22 17:17:31 +0200

    finished key codes

--- demos/ezpg.py
+++ demos/ezpg.py
@@ -10,13 +10,64 @@ h = 0
 sw = 1
 tcol = [0, 0, 0]
 
+keycodes = {
+	"A": 97,
+	"B": 98,
+	"C": 99,
+	"D": 100,
+	"E": 101,
+	"F": 102,
+	"G": 103,
+	"H": 104,
+	"I": 105,
+	"J": 106,
+	"K": 107,
+	"L": 108,
+	"M": 109,
+	"N": 110,
+	"O": 111,
+	"P": 112,
+	"Q": 113,
+	"R": 114,
+	"S": 115,
+	"T": 116,
+	"U": 117,
+	"V": 118,
+	"W": 119,
+	"X": 120,
+	"Y": 121,
+	"Z": 122,
+	"0": 48,
+	"1": 49,
+	"2": 50,
+	"3": 51,
+	"4": 52,
+	"5": 53,
+	"6": 54,
+	"7": 55,
+	"8": 56,
+	"9": 57,
+	"left": 276,
+	"right": 275,
+	"up": 273,
+	"down": 274,
+}
+
+class sketch():
+	def setup():
+		pass
+
+	def draw():
+		pass
+
+
 pygame.init()
 font = pygame.font.SysFont('Arial', 30)
 
-def start(setup, draw):
-	setup()
+def start(app):
+	app.setup()
 	while True:
-		draw()
+		app.draw()
 		__update__()
 
 def createCanvas(width, height):
@@ -97,3 +148,9 @@ def height():
 
 def mapping(n, imin, imax, omin, omax):
 	return omin+(omax-omin)*(n-imin)/(imax-imin)
+
+def isPressed(key):
+	if pygame.key.get_pressed()[keycodes[key]] == 1:
+		return True
+	else:
+		return False
